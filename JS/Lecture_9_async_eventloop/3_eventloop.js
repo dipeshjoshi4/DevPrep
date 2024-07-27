6c79074d4a66a6686cdf8dc22385e34ad15e09f5
Your Name <dipeshj2310@gmail.com>
2024-07-27 22:21:33 +0530

    add async function

--- JS/Lecture_9_async_eventloop/3_eventloop.js
+++ JS/Lecture_9_async_eventloop/3_eventloop.js
@@ -9,27 +9,34 @@ console.log("event-loop");
 // }
 // setTimeout(fn, 1000);
 // console.log("After");
-// while (a) {
+// while (a) { }
 
-// }
+//! DRY RUN OF ABOVE CODE
+
+
+
+
 
 /****
  * Browser
- * * console.log-> browser
- * * setTimout -> browser
+ //!  all consumer replay,backend connection,printing ,taking input,giving output all by browser js is just giving LOGIC
+ * * console.log-> browser -> js does not not have print idea of own language.
+ * * setTimout -> browser -> 
  * * document -> brwoser
  * * window -> browser
- * What is JS doing then 
- * Rule of thumb 
- *  JS : logic
- *  Enviornment : features
- * **/
+ * -> What is JS doing then 
+ * */
 
-/****
+ //! Rule of thumb  -----> 
+ //?  1.JS : logic
+ //?  2.Enviornment -------->  features
+
+//! Benfit of thumb rule
+/*
  * Mobile Application -> React Native -> JS
  * Frontend -> Browser -> JS
  * Backend -> Nodejs -> JS
  * Desktop App-> Electron -> JS
- * **/
+*/
 
 
